9a784fccdeebded156b405ead79c2568b3ac4939
TimLaptop <mcgowen17@yahoo.com>
2019-08-12 14:30:15 -0700

    Final comit take 2, I fixed a route error that I created by not listing the correct component

--- src/App.js
+++ src/App.js
@@ -21,12 +21,14 @@ function App() {
 	return (
 		<ProductContext.Provider value={{products, addItem}} >
 			<CartContext.Provider value={cart}>
-				<div className="App">
-					<Navigation />
-					{/* Routes */}
-					<Route exact path="/" component={Products} />
-					<Route path="/cart" component={cart} />
-				</div>
+			<div className="App">
+				<Navigation />
+
+				{/* Routes */}
+				<Route exact path="/" component={Products} />
+
+				<Route path="/cart"  component={ShoppingCart} />
+			</div>
 			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
